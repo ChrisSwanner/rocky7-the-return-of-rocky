086dc742d0589e43f7dad7a74830f8e304f07aa1
Joel Adams and Sara Hamilton <joel@>
2018-02-14 09:12:12 -0800

    add getter for player one name, add test for getter

--- RockPaperScissors.Tests/ModelTests/RockPaperScissorsTest.cs
+++ RockPaperScissors.Tests/ModelTests/RockPaperScissorsTest.cs
@@ -1,13 +1,22 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System;
-using RockPaperScissors;
+using RockPaperScissors.Models;
 
 namespace RockPaperScissors.Tests
 {
   [TestClass]
   public class RockPaperScissorsTest
   {
-
+    [TestMethod]
+    public void GetName_FetchPlayerOneName_String()
+    {
+      //arrange
+      RockPaperScissorsGame newGame = new RockPaperScissorsGame("Rock","Scissors");
+      //act
+      string result = newGame.GetPlayerOneName();
+      //assert
+      Assert.AreEqual("Player 1", result);
+    }
   }
 }
--- RockPaperScissors/Models/RockPaperScissors.cs
+++ RockPaperScissors/Models/RockPaperScissors.cs
@@ -1,10 +1,33 @@
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 using System;
 
 namespace RockPaperScissors.Models
 {
-  public class RockPaperScissorsGenerator
+  public class RockPaperScissorsGame
   {
+    private string _playerOneName;
+    private string _playerTwoName;
+    private string _playerOneChoice;
+    private string _playerTwoChoice;
+    private int _playerOneWins;
+    private int _playerTwoWins;
+    private int _draws;
 
+    public RockPaperScissorsGame (string playerOneChoice, string playerTwoChoice)
+    {
+      _playerOneName = "Player 1";
+      _playerTwoName = "Player 2";
+      _playerOneChoice = playerOneChoice;
+      _playerTwoChoice = playerTwoChoice;
+      _playerOneWins = 0;
+      _playerTwoWins = 0;
+      _draws = 0;
+    }
+
+    public string GetPlayerOneName()
+    {
+      return _playerOneName;
+    }
   }
 }
